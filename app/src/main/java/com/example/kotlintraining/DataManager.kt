d3c4e7861d23e0b73425b2ad574f52748f86bbca
henriqueschorr <henrique.schorr@gmail.com>
2020-01-28 20:53:44 -0300

    Added CardView

--- app/src/main/java/com/example/kotlintraining/DataManager.kt
+++ app/src/main/java/com/example/kotlintraining/DataManager.kt
@@ -7,7 +7,7 @@ object DataManager {
 
     init {
         initializeCourses()
-//        initializeNotes()
+        initializeNotes()
     }
 
     fun addNote(course: CourseInfo, noteTitle: String, noteText: String): Int {
@@ -25,20 +25,47 @@ object DataManager {
     }
 
     private fun initializeCourses() {
-        var course = CourseInfo("android_intents", "Android Intents");
+        var course = CourseInfo("course1", "Course 1");
         courses.set(course.courseId, course);
 
-        course = CourseInfo("adroid_async", "Android Async");
+        course = CourseInfo("course2", "Course 2");
+        courses.set(course.courseId, course);
+
+        course = CourseInfo("course3", "Course 3");
         courses.set(course.courseId, course);
     }
 
     private fun initializeNotes(){
-        var course = CourseInfo("android_intents", "Android Intents");
-        var note = NoteInfo(course, "Teste", "Testando")
+        var course = courses["course1"]
+        var note = NoteInfo(course, "Note 1", "Text 1")
         notes.add(note)
 
-        course = CourseInfo("adroid_async", "Android Async");
-        note = NoteInfo(course, "Teste2", "Testando2")
+        course = courses["course2"]
+        note = NoteInfo(course, "Note 2", "Text 2")
+        notes.add(note)
+
+        course = courses["course3"]
+        note = NoteInfo(course, "Note 3", "Text 3")
+        notes.add(note)
+
+        course = courses["course1"]
+        note = NoteInfo(course, "Note 4", "Text 4")
+        notes.add(note)
+
+        course = courses["course2"]
+        note = NoteInfo(course, "Note 5", "Text 5")
+        notes.add(note)
+
+        course = courses["course3"]
+        note = NoteInfo(course, "Note 6", "Text 6")
+        notes.add(note)
+
+        course = courses["course1"]
+        note = NoteInfo(course, "Note 7", "Text 7")
+        notes.add(note)
+
+        course = courses["course2"]
+        note = NoteInfo(course, "Note 8", "Text 8")
         notes.add(note)
     }
 
